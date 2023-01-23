cdf6fc6925f6e80db59d6d9f799d2cb3bf3e2281
Lucasvgb <lucasvgb@id.uff.br>
2023-01-23 11:19:45 -0300

    1004

--- URI/uri1004/uri1004/src/App.java
+++ URI/uri1004/uri1004/src/App.java
@@ -1,6 +1,17 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int prod = A * B;
+
+        System.out.println("PROD = "+ prod);
+        sc.close();
         
     }
 }
