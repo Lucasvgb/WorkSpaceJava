02d679f36e93302d30dfdcb5bf0110f1ca86ad51
Lucasvgb <lucasvgb@id.uff.br>
2023-01-22 23:27:50 -0300

    Adicionar pastas

---
+++ FirstProgram/primeiroPrograma/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int num1 = 10,num2 = 5,soma;
+        soma = num1 + num2;
+        System.out.print(soma);
+        
+    }
+}
