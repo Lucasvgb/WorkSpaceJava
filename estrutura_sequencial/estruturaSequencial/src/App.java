93d2a2f4621feaeeba28ea21fc48eeef54efd3d9
Lucasvgb <lucasvgb@id.uff.br>
2023-01-22 23:38:37 -0300

    Agora

--- estrutura_sequencial/estruturaSequencial/src/App.java
+++ estrutura_sequencial/estruturaSequencial/src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.print("Hello, World!");
+        System.out.print("Só para ver como vai ficar no github");
     }
 }
