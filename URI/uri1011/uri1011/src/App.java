186a21779b86ac3953cee49f2735e0729f8787fb
Lucasvgb <lucasvgb@id.uff.br>
2023-01-24 20:23:06 -0300

    uri1011

---
+++ URI/uri1011/uri1011/src/App.java
@@ -0,0 +1,17 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        
+        int raio = sc.nextInt();
+        double volume = (4 / 3.0) * 3.14159 * Math.pow(raio, 3.0);
+
+        System.out.printf("VOLUME = %.3f", volume);
+
+        sc.close();
+    }
+}
