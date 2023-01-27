d6ce185b2585b3aa8612cf72a2f0a6e2150233f7
Lucasvgb <lucasvgb@id.uff.br>
2023-01-26 22:01:06 -0300

    exercicioUri1035

---
+++ URI/uri1035/uri1035/src/App.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        Scanner input = new Scanner(System.in);
+
+        int a = input.nextInt();
+        int b = input.nextInt();
+        int c = input.nextInt();
+        int d = input.nextInt();
+
+        if(b > c && d > a && c + d > a + b && c > 0 && d > 0 && a % 2 == 0){
+            System.out.println("Valores aceitos");
+        }
+        else{
+            System.out.println("Valores nao aceitos");
+        }
+
+        input.close();
+    }
+}
