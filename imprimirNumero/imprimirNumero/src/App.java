a02c5af2ccb8c3dd45536e73f78bb0b812471cd0
Lucasvgb <lucasvgb@id.uff.br>
2023-01-26 21:46:13 -0300

    básicoSobreFor

---
+++ imprimirNumero/imprimirNumero/src/App.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+/*
+ * Imprima	todos	os	números	de	150	a	300
+ */
+public class App {
+    public static void main(String[] args) throws Exception {
+        for (int i = 150; i < 301; i++){
+            System.out.println(i);
+        }
+    }
+}
