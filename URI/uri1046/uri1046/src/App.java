40d6548d856c7b23e2067ff7c2e7f9e76a281092
Lucasvgb <lucasvgb@id.uff.br>
2023-01-31 13:26:45 -0300

    testeWhile

---
+++ URI/uri1046/uri1046/src/App.java
@@ -0,0 +1,10 @@
+
+/*
+Leia a hora inicial e a hora final de um jogo. A seguir calcule a duração do jogo, sabendo que o mesmo pode começar em um dia e terminar em outro, tendo uma duração mínima de 1 hora e máxima de 24 horas.
+ */
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+    }
+}
---
+++ exercicios/testeDeMesaWhile/testeDeMesaWhile/src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        int x = 0;
+        int y = 0;
+
+        while(x < 3){
+            y = y + 2;
+            x = x + 1;
+            System.out.println(x + "-" + y);
+        }
+       
+    }
+}
