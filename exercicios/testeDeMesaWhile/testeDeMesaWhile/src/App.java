f21436dfbfd9202f51c788b95052cbf76a0d94e9
Lucasvgb <lucasvgb@id.uff.br>
2023-02-01 12:24:02 -0300

    outros problemas

--- exercicios/testeDeMesaWhile/testeDeMesaWhile/src/App.java
+++ exercicios/testeDeMesaWhile/testeDeMesaWhile/src/App.java
@@ -9,6 +9,25 @@ public class App {
             x = x + 1;
             System.out.println(x + "-" + y);
         }
+
+        System.out.println("---------------");
+
+        int z = 5;
+        int b = 0;
+
+        while(z > 2){
+                System.out.println(z);
+                b = b + z;
+                z = z - 1;
+        }
+       System.out.println("---------------");
+
+       double u = 100;
+       double o = 100;
        
+       while( u != o){
+            System.out.println("olha");
+            u = Math.sqrt(o);
+       }
     }
 }
