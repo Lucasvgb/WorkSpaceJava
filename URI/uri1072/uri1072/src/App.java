3200f9300718f5e2d74280bc5379136e70ac0cab
Lucasvgb <lucasvgb@id.uff.br>
2023-02-08 10:34:06 -0300

    versaoFinal

--- URI/uri1072/uri1072/src/App.java
+++ URI/uri1072/uri1072/src/App.java
@@ -6,13 +6,21 @@ public class App {
         Scanner input = new Scanner(System.in);
 
         int n = input.nextInt();
-        
 
-        for(int i = 0; i <= n; i++){
-            if(n >= 10 && n <= 20){
+        int in = 0;
+        int out = 0;
 
+        for(int i = 0; i < n; i++){
+            int x = input.nextInt();
+            if(x >= 10 && x <= 20){
+                in = in + 1;
+            }
+            else{
+                out = out + 1;
             }
         }
+        System.out.println(in + " in");
+		System.out.println(out + " out");
 
         input.close();
     }
