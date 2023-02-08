5c9aa3eea6b8a8d2ae04378da06706f954bd3410
Lucasvgb <lucasvgb@id.uff.br>
2023-02-08 12:05:49 -0300

    versaoFinal

--- URI/uri1116/uri1116/src/App.java
+++ URI/uri1116/uri1116/src/App.java
@@ -1,16 +1,19 @@
+import java.util.Locale;
 import java.util.Scanner;
+import java.util.Locale;
 
 public class App {
     public static void main(String[] args) throws Exception {
         
         Scanner input = new Scanner(System.in);
-        
+        Locale.setDefault(Locale.US);
+
         int n = input.nextInt();
         for(int i = 0; i < n; i++){
             int num1 = input.nextInt();
             int num2 = input.nextInt();
             
-            if(num1 < 0 && num2 < 0){
+            if(num2 == 0){
                 System.out.println("divisao impossivel");
             }
             else{
