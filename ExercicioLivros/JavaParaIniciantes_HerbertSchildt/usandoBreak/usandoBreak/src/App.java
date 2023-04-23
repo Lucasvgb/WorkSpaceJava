ec93b5f0629c3f4e2de4287c59d3e4616990e009
Lucasvgb <lucasvgb@id.uff.br>
2023-04-23 16:10:43 -0300

    usoDoBreak

---
+++ ExercicioLivros/JavaParaIniciantes_HerbertSchildt/usandoBreak/usandoBreak/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+    int num = 100;
+
+    for(int i = 0; i < num; i++){
+        if(i * i  >= num) break;{
+            System.out.println(i + " ");
+        }
+    }
+    System.out.println("Loop Completo");
+    }
+}
